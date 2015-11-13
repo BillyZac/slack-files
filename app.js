99b1a0b7573e148ad567a36a4e60b3bbe9681d30
Zachary Smith <billyzacsmith@gmail.com>
2015-11-13 10:13:47 -0700

    Filter posts based on dropdown.

--- app.js
+++ app.js
@@ -7,45 +7,50 @@ var dataURI = 'https://slack.com/api/files.list?token=xoxp-2254406793-1272905981
 // dataURI += '&' + token + '?jsoncallback=?'
 var options = {}
 
-var tempGlobalForTesting
-
 function callback(data) {
-  for (var i=0; i<data.files.length; i++) {
-    tempGlobalForTesting = data
-    // data.files[i].name
-    var title = data.files[i].title
-    var fileType = data.files[i].pretty_type
-    // data.files[i].created
-    var thumbnail = data.files[i].thumb_360
-    var fileURL = data.files[i].permalink_public
-    // var name = data.files[i].name
+  function drawList(fileTypeSelected) {
+    // First remove all the previously drawn posts from the DOM
+    $('.post').remove()
 
-    // create div for new item
-    var $post = $('<div class="post"></div>')
-    if (fileType === 'JPEG' || fileType === 'PNG' || fileType === 'GIF') {
-      $post.append('<img class="thumbnail" src="' + thumbnail + '" alt="' + title + '">')
-    } else {
-      $post.append('<div class="fake-thumbnail"></div>')
+
+    for (var i=0; i<data.files.length; i++) {
+      // Put the useful bits of data into nice names
+      var title = data.files[i].title
+      var fileType = data.files[i].pretty_type
+      var thumbnail = data.files[i].thumb_360
+      var fileURL = data.files[i].permalink_public
+
+      // create div for new item
+      var $post = $('<div class="post"></div>')
+      if (fileType === 'JPEG' || fileType === 'PNG' || fileType === 'GIF') {
+        $post.append('<img class="thumbnail" src="' + thumbnail + '" alt="' + title + '">')
+      } else {
+        $post.append('<div class="fake-thumbnail"></div>')
+      }
+
+      // append link, title, filetype, thumbnail
+      // var $link = $('<a href="' + fileURL + '">' + title + '</a>')
+      var $link = $('<a href="' + fileURL + '">' + fileType + '</a>')
+      // var $link = title
+      $post.append($link)
+
+      console.log(fileTypeSelected)
+      if (fileTypeSelected === 'All') {
+        $('.files').append($post);
+      }
+      if (fileTypeSelected === fileType) {
+        $('.files').append($post);
+      }
     }
-
-    // append link, title, filetype, thumbnail
-    var $link = $('<a href="' + fileURL + '">' + title + '</a>')
-    // var $link = title
-    $post.append($link)
-
-
-    $('.files').append($post);
   }
+  drawList('All')
+
+  // Watch selector, and redraw the list based on the filter
+  $('select').on('input', function() {
+    var $filetype = $(this).val()
+    console.log('draw these:', $filetype)
+    drawList($filetype)
+  })
 }
 
 $.getJSON(dataURI, options, callback )
-
-function getFileTypeSelection() {
-  return $('select').val()
-}
-
-// Watch selector, (and filter the list?)
-$('select').on('input', function() {
-  var $filetype = $(this).val()
-  console.log($filetype)
-})
