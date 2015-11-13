33b2834696795e363049f5c3ba808bbcc4d9bffc
Zachary Smith <billyzacsmith@gmail.com>
2015-11-13 09:08:59 -0700

    Add a thumnail. A series of posts rather than a list. Some styles.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // billyzac
 //
 // Token:
-var token = 'xoxp-2254406793-12729059815-14447487441-6f7925dadf'
+// var token = 'xoxp-2254406793-12729059815-14447487441-6f7925dadf'
 var dataURI = 'https://slack.com/api/files.list?token=xoxp-2254406793-12729059815-14447487441-6f7925dadf&pretty=1'
 // dataURI += '&' + token + '?jsoncallback=?'
 var options = {}
@@ -11,11 +11,31 @@ var tempGlobalForTesting
 
 function callback(data) {
   for (var i=0; i<data.files.length; i++) {
-    console.log(data.files[i].permalink_public)
+    tempGlobalForTesting = data
+    // data.files[i].name
+    var title = data.files[i].title
+    var fileType = data.files[i].pretty_type
+    // data.files[i].created
+    var thumbnail = data.files[i].thumb_360
     var fileURL = data.files[i].permalink_public
-    var name = data.files[i].name
-    var newItem = $('<li><a href="' + fileURL + '"</a>' + name + '</li>')
-    $('.files').append(newItem)
+    // var name = data.files[i].name
+
+    // create div for new item
+    console.log(fileType)
+    var $post = $('<div class="post"></div>')
+    if (fileType === 'JPEG' || fileType === 'PNG' || fileType === 'GIF') {
+      $post.append('<img class="thumbnail" src="' + thumbnail + '" alt="' + title + '">')
+    } else {
+      $post.append('<div class="fake-thumbnail"></div>')
+    }
+
+    // append link, title, filetype, thumbnail
+    var $link = $('<a href="' + fileURL + '">' + title + '</a>')
+    // var $link = title
+    $post.append($link)
+
+
+    $('.files').append($post);
   }
 }
 
